c9916e03517b1c7efe85714b6fc5c6f2cddfa2c3
Nahuel <nahuelferrero@gmail.com>
2023-11-29 18:02:00 -0300

    votos con integracion

--- Votos/Votos/Program.cs
+++ Votos/Votos/Program.cs
@@ -1,27 +1,69 @@
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+// if (builder.Environment.EnvironmentName != "Development")
+// {
+//     var connection = new SqliteConnection("DataSource=votos.db");
+//     connection.Open();
+//     builder.Services.AddDbContext<VotoDb>(opt => opt.UseSqlite(connection));
+// }
+// else
+// {
+//     builder.Services.AddDbContext<VotoDb>(opt => opt.UseInMemoryDatabase("Votos"));
+// }
+
+// builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+List<Voto> _listaDeVotos = new List<Voto>();
+
+var votos = app.MapGroup("/votos");
+int contador = 0;
+
+votos.MapGet("/", () => obtenerVotos(_listaDeVotos));
+votos.MapPost("/", (Voto nuevoVoto) => crearVoto(nuevoVoto, _listaDeVotos, ref contador));
+
+static IResult obtenerVotos(List<Voto> lista)
+{
+    return TypedResults.Ok(lista);
+}
+static IResult crearVoto([FromBody] Voto nuevoVoto, List<Voto> lista, ref int contador)
+{
+    contador++;
+    nuevoVoto.Id = contador;
+    lista.Add(nuevoVoto);
+
+    return TypedResults.Ok(nuevoVoto);
+}
 
 
 app.Run();
 
-class Voto
+public class Voto
 {
-    int Id {get; set;}
-    Votante Persona {get; set;}
-    Candidato Eleccion {get; set;}
+    public int Id {get; set;}
+    public required int IdVotante {get; set;}
+    public int? IdCandidato {get; set;}
 }
 
-class Candidato
+public class Votante
 {
-    public int Id {get; set;}
-    public string? Nombre {get; set;}
-    public string? Apellido {get; set;}
-    public string? Partido {get; set;}
+    [JsonPropertyName("id")] public int Id { get; set; }
+    [JsonPropertyName("fechaNacimiento")] public required string FechaNacimiento { get; set; }
 }
-class Votante
+
+public class VotoDb : DbContext
 {
-    public int Id {get; set;}
-    public string? Nombre {get; set;}
-    public string? Apellido {get; set;}
-    public int? Dni {get; set;}
+    public VotoDb(DbContextOptions<VotoDb> options) : base(options) { 
+        Database.EnsureCreated();
+    }
+
+    public DbSet<Voto> Votos => Set<Voto>();
 }
